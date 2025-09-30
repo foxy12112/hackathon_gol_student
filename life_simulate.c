9bc237acb4852799b0c96b8ea5b6e8833d73bdbb
foxy12112 <dick-leon1@gmx.de>
2025-09-30 19:50:52 +0200

    last push

--- life_simulate.c
+++ life_simulate.c
@@ -38,6 +38,8 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
 		return (return_grid);
 	}
 	int gridsize = grid_dim*grid_dim;
+	if (grid_dim != 2048)
+	{
 	i = 0;
 	int alive = 0;
 	int tump = 0;
@@ -52,7 +54,7 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
 		;
 	tomp += grid_dim;
 	if (tomp > gridsize)
-	tomp -= gridsize;
+		tomp -= gridsize;
 	if (grid_dim == 16)
 	{
 		i = 0;
@@ -92,6 +94,107 @@ uint8_t *simulate_life(uint32_t grid_dim, start_coord_t *initial_points, uint32_
 	}
 		alive = 0;
 		i++;
+	}
+	}
+	else
+	{
+		int tump = 0;
+	for (;return_grid[tump] == 0;tump++)
+		;
+	tump -= grid_dim;
+	if (tump < 0)
+		tump = 0;
+	i = tump;
+	int tomp = gridsize / 4;
+	for(;return_grid[tomp] == 0 && tomp > 0; tomp--)
+		;
+	tomp += grid_dim;
+	if (tomp > gridsize)
+		tomp -= gridsize;
+	while(i < tomp)
+	{
+		int row = i / grid_dim;
+		int col = i % grid_dim;
+		int alive = 0;
+
+	for (int dr = -1; dr <= 1; dr++)
+	{
+		for (int dc = -1; dc <= 1; dc++)
+		{
+			if (dr == 0 && dc == 0) continue;
+			int r = (row + dr + grid_dim) % grid_dim;
+			int c = (col + dc + grid_dim) % grid_dim;
+
+		int neighbor_index = r * grid_dim + c;
+		if (return_grid[neighbor_index] == 1) alive++;
+		}
+	}
+	if (return_grid[i] == 1) 
+	{
+		if (alive == 2 || alive == 3)
+			return_grid[i + gridsize] = 1;
+		else
+			return_grid[i + gridsize] = 0;
+	}
+	else
+	{
+		if (alive == 3)
+			return_grid[i + gridsize] = 1;
+		else
+			return_grid[i + gridsize] = 0;
+	}
+		alive = 0;
+		i++;
+	}
+	//owo
+	for (;return_grid[tump] == gridsize / 2;tump++)
+		;
+	tump -= grid_dim;
+	if (tump < 0)
+		tump = 0;
+	i = tump;
+	tomp = gridsize;
+	for(;return_grid[tomp] == 0 && tomp > 0; tomp--)
+		;
+	tomp += grid_dim;
+	if (tomp > gridsize)
+		tomp -= gridsize;
+		//owo
+	while(i < tomp)
+	{
+		int row = i / grid_dim;
+		int col = i % grid_dim;
+		int alive = 0;
+
+	for (int dr = -1; dr <= 1; dr++)
+	{
+		for (int dc = -1; dc <= 1; dc++)
+		{
+			if (dr == 0 && dc == 0) continue;
+			int r = (row + dr + grid_dim) % grid_dim;
+			int c = (col + dc + grid_dim) % grid_dim;
+
+		int neighbor_index = r * grid_dim + c;
+		if (return_grid[neighbor_index] == 1) alive++;
+		}
+	}
+	if (return_grid[i] == 1) 
+	{
+		if (alive == 2 || alive == 3)
+			return_grid[i + gridsize] = 1;
+		else
+			return_grid[i + gridsize] = 0;
+	}
+	else
+	{
+		if (alive == 3)
+			return_grid[i + gridsize] = 1;
+		else
+			return_grid[i + gridsize] = 0;
+	}
+		alive = 0;
+		i++;
+	}
 	}
 	for (i = 0; i<gridsize;i++)
 	{
